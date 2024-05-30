d6f25fb523ed6989d2f8d487dc1511270e13a9f5
Matt Batman <batman.matt@gmail.com>
2024-05-30 11:15:03 -0400

    Verify token in middleware

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -12,7 +12,7 @@ const createJWT = (user) => {
   return token;
 };
 
-const protect = (req, res) => {
+const protect = (req, res, next) => {
   const bearer = req.headers.authorization;
 
   if (!bearer) {
@@ -25,7 +25,19 @@ const protect = (req, res) => {
 
   if (!token) {
     res.status(401);
-    res.json({ message: "not authorized" });
+    res.json({ message: "not valid token" });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401);
+    res.json({ message: "not valid token" });
     return;
   }
 };
