2a6677d8f26afef425c0da8efd76fd4c1ab38ea6
Matt Batman <batman.matt@gmail.com>
2024-05-30 11:10:55 -0400

    Check for token

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -20,6 +20,14 @@ const protect = (req, res) => {
     res.json({ message: "not authorized" });
     return;
   }
+
+  const [, token] = bearer.split(" ");
+
+  if (!token) {
+    res.status(401);
+    res.json({ message: "not authorized" });
+    return;
+  }
 };
 
 export { createJWT, protect };
